fa0d40e93438361d55e16ff2453329e2ed0e0777
Roni Frumkis <roni.frumkis@post.runi.ac.il>
2025-12-30 18:28:27 +0200

    finish 2

--- Palindrome.java
+++ Palindrome.java
@@ -7,7 +7,13 @@ public class Palindrome {
 	
 	/** Checks if the given string is a palindrome. */
 	public static boolean isPalindrome(String s) {
-		//// Replace the followaing statement with your code
-		return false;
-    }
+		int N = s.length();
+		if ((s.length() == 0) || (s.length() == 1)) {
+			return true;
+		}
+		if (s.charAt(0) != s.charAt(N-1)) {
+			return false;
+		}
+		return isPalindrome (s.substring(1, N - 1));  
+    }    
 }
