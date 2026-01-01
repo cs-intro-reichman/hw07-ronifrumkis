28ce17132ecd640d0eb66fd10115f8e044a9a7cb
Roni Frumkis <roni.frumkis@post.runi.ac.il>
2026-01-01 11:47:16 +0200

    finish 5

--- IsSorted.java
+++ IsSorted.java
@@ -2,15 +2,24 @@ public class IsSorted {
 
     // Public function: called by the user
     public static boolean isSorted(int[] a) {
+        if (a.length <= 1) {
+            return true;
+        }
         return isSorted(a, 0);
     }
 
     // Helper recursive function
     private static boolean isSorted(int[] a, int i) {
         //// Replace the following statement with your code
-        return false; 
+        if (i == a.length - 1) {
+            return true;
+        }
+        if (a[i] > a[i+1]) {
+            return false;
+        }
+        return isSorted(a, i + 1);
     }
-
+        
     public static void main(String[] args) {
         int[] a = {1, 2, 2, 5};
         System.out.println(isSorted(a)); // true
