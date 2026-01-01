2828d0413723d2e0d0cdec7e312f1490c8e5c236
Roni Frumkis <roni.frumkis@post.runi.ac.il>
2026-01-01 11:38:23 +0200

    finish 2

--- Binomial.java
+++ Binomial.java
@@ -18,8 +18,8 @@ public class Binomial {
 	}
 	
 	// Computes the Binomial function, efficiently
-	public static int binomial(int n, int k) {
-		int [][] memo = new int [n+1][k+1];
+	public static long binomial(int n, int k) {
+		long [][] memo = new long [n+1][k+1];
 		for (int i = 0 ; i<= n ; i++) {
 			for (int j = 0 ; j <= k ; j++) {
 				memo [i][j] = -1;
@@ -35,7 +35,7 @@ public class Binomial {
 		
 	
 
-	private static int binomial(int n, int k, int[][] memo) {
+	private static long binomial(int n, int k, long[][] memo) {
 		if (memo[n][k] != -1) {
 			return memo[n][k];
 		}
